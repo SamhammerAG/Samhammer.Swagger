2c0c17dd2dbc3548446b7811a779d0016bda60d6
Roland Schmitt <rschmitt@samhammer.de>
2022-01-19 17:21:26 +0100

    ASMO-3954 mark Samhammer.Swagger.Default as obsolete #2

--- src/Samhammer.Swagger.Default/SwaggerExtensions.cs
+++ src/Samhammer.Swagger.Default/SwaggerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
@@ -7,6 +8,7 @@ namespace Samhammer.Swagger.Default
 {
     public static class SwaggerExtensions
     {
+        [Obsolete("swashbuckle 6.2.3 setups default path for swagger.json for swagger-gen and swagger-ui already")]
         public static void AddSwaggerDefaultApi(this IServiceCollection services)
         {
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGen>();
